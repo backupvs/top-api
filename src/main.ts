0ca7727367f56a064906f4c08f3ee31c2a17ba31
backupvs <dmitryholubev@gmail.com>
2023-04-16 19:41:21 +0300

    Added global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
+
+  // Global pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true
+      }
+    })
+  );
+
   await app.listen(port);
   console.log(`Listening on ${port}`);
 }
