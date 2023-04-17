6b6ff8bd113def5392f9cc9b3347f7f348911028
backupvs <dmitryholubev@gmail.com>
2023-04-17 21:54:41 +0300

    Update getMongoString considering prod mode

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -9,16 +9,16 @@ export const getMongoConfig = async (configService: ConfigService): Promise<Type
 };
 
 const getMongoString = (configService: ConfigService) => {
-  return 'mongodb://' +
-    configService.get('MONGO_LOGIN') +
-    ':' +
-    configService.get('MONGO_PASSWORD') +
-    '@' +
-    configService.get('MONGO_HOST') +
-    ':' +
-    configService.get('MONGO_PORT') +
-    '/' +
-    configService.get('MONGO_AUTHDATABASE')
+  const isProd = process.env.NODE_ENV === 'production';
+
+  const protocol = isProd ? 'mongodb+srv' : 'mongodb';
+  const login = configService.get('MONGO_LOGIN');
+  const password = configService.get('MONGO_PASSWORD');
+  const host = configService.get('MONGO_HOST');
+  const port = isProd ? '' : `:${configService.get('MONGO_PORT')}`;
+  const authDatabase = configService.get('MONGO_AUTHDATABASE');
+
+  return `${protocol}://${login}:${password}@${host}${port}/${authDatabase}`;
 }
 
 const getMongoOptions = () => ({
